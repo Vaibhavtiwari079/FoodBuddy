0ab87463f47f1419a4403a94d7d2267cb7e4ec07
Vaibhavtiwari079 <vaibhavtiwari.0504@gmail.com>
2024-08-01 12:00:52 +0530

    Starting the project

---
+++ src/app/components/RestaurantLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantLogin() {
+  return (
+    <div>RestaurantLogin</div>
+  )
+}
+
+export default RestaurantLogin
---
+++ src/app/components/RestaurantSignup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantSignup() {
+  return (
+    <div>RestaurantSignup</div>
+  )
+}
+
+export default RestaurantSignup
